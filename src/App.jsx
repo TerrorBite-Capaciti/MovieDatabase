84fb6769500fae435e363d965d0aad3c0c29889a
LefaJele-Masemola <lefa030205@gmail.com>
2024-11-26 14:41:24 +0200

    added routing for movies and defined mock data for movies

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,35 @@ import { Route, Routes } from "react-router-dom";
 import SplashScreen from "./components/SplashScreen";
 import HomeScreen from "./pages/HomeScreen";
 import SearchResults from "./pages/SearchResults";
+import MovieCard from "./MovieCard/MovieCard";
 
+//defined mock data for movies
+const mockMovies = [
+  {
+    id: 1,
+    title: "Inception",
+    image: "https://image.tmdb.org/t/p/w500/8h58BL2Fg9Rlj5tBXGojJ3LgNkJ.jpg",
+  },
+  {
+    id: 2,
+    title: "The Dark Knight",
+    image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    image: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
+  },
+];
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SplashScreen />} />
       <Route path="/home" element={<HomeScreen />} />
       <Route path="/search" element={<SearchResults />} />
+
+    {/* Route for Movie Card */}
+        <Route path="/movies" element={<MovieCard movies={mockMovies} />} /> 
     </Routes>
   );
 };
